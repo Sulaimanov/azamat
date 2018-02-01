4af3bc2da3dcba07ce534c7d300344875f78b5af
RushianHaker <ivanov2001m352g@mail.ru>
2018-02-01 12:50:23 +0300

    project.start

---
+++ azamat/view Controller3.swift
@@ -0,0 +1,25 @@
+//
+//  view Controller3.swift
+//  azamat
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+
+import UIKit
+
+class view3ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
